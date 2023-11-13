5e77bfce1ba76bbbf0dafabc2a90c653a58b6d1a
uarif <uarif@users.noreply.github.com>
2023-11-13 01:51:21 -0500

    Update inverse_fft.py
    Docstrings added..

--- Task2/inverse_fft.py
+++ Task2/inverse_fft.py
@@ -9,6 +9,15 @@ data = pd.read_csv(
 y = df['value'].values
 # FFT and IFFT functions
 def fft(x):
+    """
+    Perform Fast Fourier Transform (FFT) on the input signal.
+
+    Parameters:
+    - x (array-like): Input signal.
+
+    Returns:
+    - array-like: FFT result.
+    """
     N = len(x)
     if N <= 1: return x
     even = fft(x[0::2])
@@ -17,6 +26,15 @@ def fft(x):
     return [even[k] + T[k] for k in range(N // 2)] + [even[k] - T[k] for k in range(N // 2)]
 
 def ifft(x):
+    """
+    Perform Inverse Fast Fourier Transform (IFFT) on the input signal.
+
+    Parameters:
+    - x (array-like): Input signal.
+
+    Returns:
+    - array-like: IFFT result.
+    """
     x_conj = np.conj(x)
     x_fft = fft(x_conj)
     x_fft_conj = np.conj(x_fft)
