f292e537d9e279a0f0463ffd367b80e83eda0963
Pranjal Srivastava <143828394+Pranjal-Srivastava-2023@users.noreply.github.com>
2023-11-14 20:56:51 -0500

    Added docstrings

--- calculate_frequency.py
+++ calculate_frequency.py
@@ -1,10 +1,31 @@
-def calculate_frequencies(X):
-    sample_spacing = 1  # assuming monthly data, so the spacing is 1 month
+import numpy as np
+
+def calculate_frequencies(X, sample_spacing=1):
+    """
+    Calculate the frequencies corresponding to the FFT output.
+
+    Parameters:
+    - X (array-like): The output of the FFT.
+    - sample_spacing (float, optional): The spacing between data points. Defaults to 1 (assuming monthly data).
+
+    Returns:
+    - array-like: Frequencies corresponding to the FFT output.
+    """
     N = len(X)
     freqs = np.fft.fftfreq(N, d=sample_spacing)
     return freqs[:N//2]  # return only the positive frequencies
 
 def identify_peak_frequency(frequencies, X):
+    """
+    Identify the peak frequency from the FFT output.
+
+    Parameters:
+    - frequencies (array-like): Frequencies corresponding to the FFT output.
+    - X (array-like): The output of the FFT.
+
+    Returns:
+    - float: The peak frequency.
+    """
     # Find the index of the peak frequency
     peak_index = np.argmax(np.abs(X[:len(frequencies)]))
     return frequencies[peak_index]
