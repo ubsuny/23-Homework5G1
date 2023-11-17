f615c82f1ae16ddd8dd6b80b73a6eb15d9db9553
tirthbha <143649367+tirthbha@users.noreply.github.com>
2023-11-17 00:17:44 -0500

    modified windowing_padding.py

--- Task2/windowing_padding.py
+++ Task2/windowing_padding.py
@@ -1,37 +1,52 @@
+from fft import *
+import math
 import numpy as np
-from inverse_fft import *
+import pandas as pd
 import matplotlib.pyplot as plt
-# Padding
-log2N = np.log2(len(y))
-next_pow_of_2 = int(log2N) + 1
-if log2N != int(log2N):
-    y_padded = np.pad(y, (0, 2**next_pow_of_2 - len(y)), 'constant', constant_values=(0,))
-else:
-    y_padded = y
-x_padded = np.arange(len(y_padded))
+window = True
 
-# Windowing
-window = 0.5 - 0.5 * np.cos(2 * np.pi * x_padded / (len(y_padded) - 1))
-y_windowed = y_padded * window
+df = pd.read_csv(
+    'https://gml.noaa.gov/aftp/data/trace_gases/co2/flask/surface/txt/co2_mid_surface-flask_1_ccgg_month.txt',
+    delimiter="\s+",skiprows=54, names=['site',	'year',	'month',	'value'])
 
-# FFT
-Y = fft(y_windowed)
+# Read like previous example with CO2 data
+y = df['value'].values
+y_valid = y >= 0.
+y = y[y_valid]
 
-# Filtering (e.g., remove high frequencies)
-maxfreq = 5
-Y_filtered = Y.copy()
-Y_filtered[maxfreq:len(Y)-maxfreq] = np.zeros(len(Y)-2*maxfreq)
+# instead of truncating, pad with values
 
-# Inverse FFT
-y_filtered = ifft(Y_filtered)
-y_filtered_abs = np.abs(y_filtered)
+M = len(y)
+log2M = math.log(M, 2)
+next_pow_of_2 = int(log2M) + 1
+if log2M - int(log2M) > 0.0 :    
+    ypads = np.full( 2**( next_pow_of_2) - M, 0, dtype=np.double)
+    y = np.concatenate( (y, ypads) )
+    # CAREFUL: When you pad, the x axis becomes somewhat "meaningless" for the padded values, 
+    # so typically it is best to just consider it an index
+    x = np.arange(len(y))
+    M = len(y)
+    # Apply a window to reduce ringing from the 2^n cutoff
+    if window : 
+        y = y * (0.5 - 0.5 * np.cos(2*np.pi*x/(M-1)))
+                
+Y = fft(y)
+Y_abs = abs(Y)
+powery = fft_power(Y)
+powerx = np.arange(powery.size)
 
-# Plotting
-# CO2 concentration plot
-plt.figure(figsize=(12, 8))
-plt.subplot(2, 1, 1)
-plt.plot(y_windowed, label='windowed data')
-plt.plot(y[:len(y)], label='Original')  # Plotting only the original length part
-plt.xlabel("Month Index")
-plt.ylabel("CO2 Concentration (ppm)")
+f1 = plt.figure(1)
+plt.plot( x, y )
+plt.xlabel("Index")
+plt.ylabel("CO$_2$ Concentration")
+
+f2 = plt.figure(2)
+plt.plot( powerx, powery, label="Power" )
+plt.plot( x, Y_abs, label="Magnitude" )
+plt.xlim([0,M/4])
 plt.legend()
+plt.yscale('log')
+plt.xlabel("Spectral Index")
+plt.ylabel("Fourier Component")
+
+plt.show()
